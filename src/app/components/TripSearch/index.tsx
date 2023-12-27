14c3c6d0eba7d2424c41cea78d5f9f2878787e5d
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-27 19:09:55 -0300

    feat: Adicionando DatePicker component no TripSearch components

--- src/app/components/TripSearch/index.tsx
+++ src/app/components/TripSearch/index.tsx
@@ -1,9 +1,23 @@
+"use client"
+
+import DatePicker from "@/components/DatePicker";
+import Input from "@/components/Input";
+
 const TripSearch = () => {
   return (
     <div className="container mx-auto p-5">
-      <h1 className=" font-semibold text-2xl text-primaryDarker text-center">
+      <h1 className=" text-center text-2xl font-semibold text-primaryDarker">
         Encontre sua próxima <span className="text-primary">viagem!</span>
       </h1>
+
+      <div className="mt-5 flex flex-col gap-4">
+        <Input placeholder="Onde você quer ir?" />
+
+        <div className="flex gap-4">
+          <DatePicker placeholderText="Data de ida" onChange={() => {}} className="w-full"/>
+          <Input placeholder="Orçamento" />
+        </div>
+      </div>
     </div>
   );
 };
