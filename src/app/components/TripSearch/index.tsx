5b35444661d28c414993ab93e5354b399a43bb30
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-27 18:45:25 -0300

    feat: Adicionando o Input component, criando o TripSearch componente e adicionando-o na Home Page

---
+++ src/app/components/TripSearch/index.tsx
@@ -0,0 +1,11 @@
+const TripSearch = () => {
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className=" font-semibold text-2xl text-primaryDarker text-center">
+        Encontre sua próxima <span className="text-primary">viagem!</span>
+      </h1>
+    </div>
+  );
+};
+
+export default TripSearch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import TripSearch from "./components/TripSearch"
 
 export default function Home() {
   return (
     <div>
-      
+      <TripSearch />
     </div>
   )
 }
