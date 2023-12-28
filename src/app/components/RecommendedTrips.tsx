a37a68e378bf4aecc62b1ed2110d12172e636fa0
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-27 21:05:10 -0300

    feat: Adicionando TripItem components e RecommendedTrips na Home Page

--- src/app/components/RecommendedTrips.tsx
+++ src/app/components/RecommendedTrips.tsx
@@ -1,9 +1,33 @@
-const RecommendedTrips = () => {
-    return ( 
-        <div>
-            Recomendados
-        </div>
-     );
+import TripItem from "@/components/TripItem";
+import { prismaCliente } from "@/lib/prisma";
+import { Trip } from "@prisma/client";
+
+async function getTrips() {
+  const trips = await prismaCliente.trip.findMany({});
+
+  return trips;
 }
- 
+
+const RecommendedTrips = async () => {
+  const data = await getTrips();
+
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex items-center">
+        <div className="h-[1px] w-full bg-grayLighter"></div>
+        <h2 className="whitespace-nowrap px-5 font-medium text-grayPrimary">
+          Destinos Recomendados
+        </h2>
+        <div className="h-[1px] w-full bg-grayLighter"></div>
+      </div>
+
+      <div className="mt-5 flex flex-col items-center gap-5">
+        {data.map((trip: Trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default RecommendedTrips;
---
+++ src/components/TripItem.tsx
@@ -0,0 +1,40 @@
+import { Trip } from "@prisma/client";
+import Image from "next/image";
+import ReactCountryFlag from "react-country-flag";
+
+interface TripItemProps {
+  trip: Trip;
+}
+
+const TripItem = ({ trip }: TripItemProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[280px] w-[280px]">
+      <Image
+        src={trip.coverImage}
+        className="rounded-lg shadow-md"
+        style={{
+          objectFit: "cover"
+        }}
+        alt={trip.name}
+        fill
+      />
+      </div>
+      <h3 className=" mt-2 text-sm font-medium text-primaryDarker">
+        {trip.name}
+      </h3>
+      <div className="flex items-center gap-1 my-1">
+        <ReactCountryFlag countryCode={trip.countryCode} svg />
+        <p className="text-xs text-grayPrimary">{trip.location}</p>
+      </div>
+      <p className="text-xs text-grayPrimary">
+        <span className="font-medium text-primary">
+          R$ {trip.pricePerDay.toString()}
+        </span>{" "}
+        por dia
+      </p>
+    </div>
+  );
+};
+
+export default TripItem;
