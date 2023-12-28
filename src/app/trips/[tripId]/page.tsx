9c0ce307f90f5056fb1a61d0faa21302ac1a959c
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-28 13:04:07 -0300

    feat: Adicionando coverImage na trip page

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,9 +1,33 @@
-const TripDetails = ({params}: {params : {tripId: string}}) => {
-    return ( 
-        <div>
-            Detalhes da {params.tripId}
-        </div>
-     );
-}
- 
+import { prismaCliente } from "@/lib/prisma";
+import Image from "next/image";
+
+const getTripDetails = async (tripId: string) => {
+  const trip = await prismaCliente.trip.findUnique({
+    where: {
+      id: tripId,
+    },
+  });
+
+  return trip;
+};
+
+const TripDetails = async ({ params }: { params: { tripId: string } }) => {
+  const trip = await getTripDetails(params.tripId);
+
+  if (!trip) return null;
+
+  return (
+    <div className="container">
+      <div className="relative h-[300px] w-full">
+        <Image
+          src={trip?.coverImage}
+          fill
+          style={{ objectFit: "cover" }}
+          alt={trip.name}
+        />
+      </div>
+    </div>
+  );
+};
+
 export default TripDetails;
