d0d75aa47885c06bc2fb035f9f026f91093ab97b
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-28 13:48:28 -0300

    feat: Adicionando TripHighlights components

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -2,6 +2,7 @@ import { prismaCliente } from "@/lib/prisma";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prismaCliente.trip.findUnique({
@@ -23,6 +24,7 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
       <TripHeader trip={trip} />
       <TripReservation trip={trip} />
       <TripDescription description={trip.description}/>
+      <TripHighlights highlights={trip.highlights} />
     </div>
   );
 };
