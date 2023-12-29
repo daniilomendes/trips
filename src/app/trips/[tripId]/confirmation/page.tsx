cc8cc5911a4d290fe64c95e9b4ac5ff1ee966881
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-29 13:31:49 -0300

    feat: Garantindo a validação de autenticação

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -8,10 +8,16 @@ import { useEffect, useState } from "react";
 import ReactCountryFlag from "react-country-flag";
 import ptBR from "date-fns/locale/pt-BR";
 import Button from "@/components/Button";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>();
   const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  const router = useRouter();
+
+  const { status } = useSession();
 
   const searchParams = useSearchParams();
 
@@ -32,14 +38,18 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
       setTotalPrice(totalPrice);
     };
 
+    if (status === "unauthenticated") {
+      router.push("/")
+    }
+
     fetchTrip();
-  }, []);
+  }, [status]);
 
   if (!trip) return null;
 
   const startDate = new Date(searchParams.get("startDate") as string);
   const endDate = new Date(searchParams.get("endDate") as string);
-  const guests = searchParams.get("guests")
+  const guests = searchParams.get("guests");
 
   return (
     <div className="container mx-auto p-5">
@@ -89,7 +99,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
           <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
         </div>
 
-        <h3 className="font-semibold mt-5">Hóspedes</h3>
+        <h3 className="mt-5 font-semibold">Hóspedes</h3>
         <p>{guests} hóspedes</p>
 
         <Button className="mt-5">Finalizar Compra</Button>
