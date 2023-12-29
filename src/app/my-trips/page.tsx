6046a6c05b813f12cf12befac90b0f311ab75db3
Danilo Mendes <danilo.mirandha@gmail.com>
2023-12-29 15:04:17 -0300

    feat: adicionando rota para pegar as reservas do usuario

---
+++ src/app/my-trips/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { TripReservation } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const MyTrips = () => {
+  const [reservations, setReservations] = useState<TripReservation[]>([]);
+
+  const { status, data } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated" || !data?.user) {
+      return router.push("/");
+    }
+
+    const fetchReservations = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`,
+      );
+      const json = await response.json();
+
+      setReservations(json);
+    };
+
+    fetchReservations();
+  }, [status]);
+
+  return <div className="">My trips</div>;
+};
+
+export default MyTrips;
